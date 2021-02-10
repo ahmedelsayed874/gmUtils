8fbd45eacd32a686737619bd38c69c60763151d1
Ahmed El-Sayed <ahmed_abdo@ogtech.com>
2021-02-10 16:13:13 +0200

    .

--- GMUtilsAndroid/GMUtils/src/main/java/com/blogspot/gm4s1/gmutils/database/BaseDatabase.java
+++ GMUtilsAndroid/GMUtils/src/main/java/com/blogspot/gm4s1/gmutils/database/BaseDatabase.java
@@ -33,6 +33,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * Created by Ahmed El-Sayed (Glory Maker)
+ * Computer Engineer / 2012
+ * Android/iOS Developer with (Java/Kotlin, Swift)
+ * Have experience with:
+ * - (C/C++, C#) languages
+ * - .NET environment
+ * - AVR Microcontrollers
+ * a.elsayedabdo@gmail.com
+ * +201022663988
+ */
 interface DatabaseCallbacks {
     void onCreate(SQLiteDatabase db);
 
