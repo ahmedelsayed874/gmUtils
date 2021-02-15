9e5fa79ef574ab0749a45e43833080695ada4858
ahmed_hammouda_ogtech <ahmed0012012>
2021-02-15 22:57:03 +0200

    .

---
+++ GMUtilsAndroid/app/src/main/java/com/blogspot/gm4s/gmutileexample/MapAndLocationExample.kt
@@ -0,0 +1,251 @@
+package com.blogspot.gm4s.gmutileexample
+
+import android.Manifest
+import android.accounts.Account
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.location.Location
+import android.location.LocationManager
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import com.blogspot.gm4s.gmutileexample.R
+import com.blogspot.gm4s1.gmutils.Activities
+import com.blogspot.gm4s1.gmutils.ImageLoader
+import com.blogspot.gm4s1.gmutils.Logger
+import com.blogspot.gm4s1.gmutils.geography.LocationTracker
+import com.blogspot.gm4s1.gmutils.geography.MapController
+import com.blogspot.gm4s1.gmutils.storage.AccountStorage
+import com.blogspot.gm4s1.gmutils.ui.MyToast
+import com.blogspot.gm4s1.gmutils.ui._bases.BaseActivity
+import com.blogspot.gm4s1.gmutils.ui._bases.BaseViewModel
+import com.blogspot.gm4s1.gmutils.ui.dialogs.MessageDialog
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Marker
+import org.w3c.dom.Text
+import java.io.File
+import java.util.HashMap
+
+class MapAndLocationExample : BaseActivity() {
+
+    companion object {
+        fun start(context: Context) {
+            Activities.start(MapAndLocationExample::class.java, context, null)
+        }
+
+        const val REQUEST_CODE_LOCATION = 1
+    }
+
+    private var requiredPermissionGranted = false
+    private var mMapController: MapController? = null
+    private var mLocationTracker: LocationTracker? = null
+
+    private var id = 1
+
+    override fun getActivityLayout() = R.layout.activity_map_and_location
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        checkRequiredPermissions()
+
+        initUI()
+
+    }
+
+    private fun checkRequiredPermissions() {
+        val notGranted1 = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+        val notGranted2 = ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED
+
+        if (notGranted1 && notGranted2) {
+            requiredPermissionGranted = false
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ), REQUEST_CODE_LOCATION
+            )
+        } else {
+            requiredPermissionGranted = true
+        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    override fun onStart() {
+        super.onStart()
+
+        //region check required permissions
+        checkRequiredPermissions()
+        //endregion
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requiredPermissionGranted = true
+                setMyLocationButtonEnabled()
+                setupLocationTracker()
+
+            } else {
+                showEnableGPSAlert()
+            }
+        }
+    }
+
+    private fun showEnableGPSAlert() {
+        MessageDialog.create(this)
+            .setMessage(/*R.string.*/"we need you to enable gps to enable us get your physical location")
+            .setButton1(R.string.ok) {
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+            .setButton2(R.string.cancel, null)
+            .show()
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    //region UI aspect
+
+    private fun initUI() {
+        setupMap()
+
+        setupLocationTracker()
+    }
+
+    private fun setupMap() {
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mMapController = MapController(mapFragment)
+        mMapController?.zoom(15f)
+        mMapController?.enableZoomControl()
+        //mMapController?.enableToolbarControl()
+
+        mMapController?.setInfoWindowAdapter(object : GoogleMap.InfoWindowAdapter {
+            override fun getInfoWindow(p0: Marker?): View? {
+                return null
+            }
+
+            override fun getInfoContents(p0: Marker?): View? {
+                val info = p0?.tag as? String
+                return createMapInfoWindowView(info)
+            }
+
+        })
+        mMapController?.setOnInfoWindowClickListener {
+            val info = it.tag as String
+            MyToast.show(thisActivity(), info)
+        }
+
+        setMyLocationButtonEnabled()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationButtonEnabled() {
+        if (requiredPermissionGranted) {
+//            mMapController?.showMyLocationButton()
+        }
+    }
+
+    private fun createMapInfoWindowView(info: String?): View? {
+        if (info != null) {
+            val view = TextView(this)
+            view.setText(info)
+            view.setBackgroundResource(R.drawable.shape_solid_round_accent)
+            view.setPadding(15, 15, 15, 15)
+
+            return view
+        }
+        return null
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    @SuppressLint("MissingPermission")
+    private fun setupLocationTracker() {
+        if (!requiredPermissionGranted) return
+        mLocationTracker = LocationTracker(this, object : LocationTracker.Listener {
+            override fun onLocationFounded(obj: LocationTracker?, location: Location) {
+                val pin = MapController.MapPin(
+                    id.toString(), //important for tracking
+                    location.latitude,
+                    location.longitude
+                ).also {
+                    it.setName(AccountStorage.ACCOUNT._full_name)
+                    it.setExtraData(AccountStorage.ACCOUNT)
+                    //it.setIcon()
+                }
+
+                mMapController?.addMarker(pin)
+
+            }
+
+            override fun onLocationProviderStatusChanged(
+                p0: LocationTracker?,
+                p1: String?,
+                disabled: Boolean
+            ) {
+                if (disabled) {
+                    if (LocationManager.GPS_PROVIDER == p1) {
+                        showEnableGPSAlert()
+                    }
+                }
+            }
+
+            override fun onErrorHappened(p0: LocationTracker, p1: Int, p2: String?) {
+                when (p1) {
+                    LocationTracker.Listener.ERR_GPS_CLOSED ->
+                        p0.showGPSDisabledAlert(
+                            thisActivity(),
+                            getString(R.string.app_name),
+                            /*getString(R.string.*/
+                            "please_enable_GPS_to_help_us_find_your_physical_location",
+                            /*getString(R.string.*/"location_settings",
+                            "cancel",
+                            null
+                        )
+
+                    LocationTracker.Listener.ERR_EXCEPTION ->
+                        Logger.print(p2)
+                }
+            }
+        })
+        mLocationTracker?.startLocationUpdating()
+    }
+
+//endregion
+
+//----------------------------------------------------------------------------------------------
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        mMapController?.destroy()
+        mMapController = null
+
+    }
+
+
+}
