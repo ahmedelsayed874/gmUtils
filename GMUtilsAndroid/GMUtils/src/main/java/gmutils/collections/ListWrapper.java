59f4bdd120e82d4535d7dc09194bbf20eb08895c
ahmed_hammouda_ogtech <ahmed0012012>
2021-07-09 12:13:03 +0200

    introduce new class that wraps list & map to help in inline the code and make it more readable.

---
+++ GMUtilsAndroid/GMUtils/src/main/java/gmutils/collections/ListWrapper.java
@@ -0,0 +1,140 @@
+package gmutils.collections;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import gmutils.listeners.ActionCallback;
+import gmutils.listeners.ActionCallback2;
+import gmutils.listeners.ResultCallback;
+
+public class ListWrapper<T> {
+
+    public static <T> ListWrapper<T> create(Class<T> cls) {
+        return new ListWrapper<>(null);
+    }
+
+    public static <T> ListWrapper<T> create(List<T> list) {
+        return new ListWrapper<>(list);
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    private final List<T> list;
+
+    private ListWrapper(List<T> list) {
+        if (list == null) {
+            this.list = new ArrayList<>();
+        } else {
+            this.list = list;
+        }
+    }
+
+    //region add methods
+    public ListWrapper<T> add(T item) {
+        this.list.add(item);
+        return this;
+    }
+
+    public ListWrapper<T> add(T item, int at) {
+        this.list.add(at, item);
+        return this;
+    }
+
+    public ListWrapper<T> add(List<T> items) {
+        this.list.addAll(items);
+        return this;
+    }
+
+    public ListWrapper<T> add(List<T> items, int at) {
+        this.list.addAll(at, items);
+        return this;
+    }
+
+    public ListWrapper<T> add(int count, ActionCallback<Integer, T> adder) {
+        if (count == 0 || adder == null) return this;
+
+        for (int i = 0; i < count; i++) {
+            T item = adder.invoke(i);
+            add(item);
+        }
+
+        return this;
+    }
+    //endregion
+
+    //region remove methods
+    public ListWrapper<T> remove(T item) {
+        this.list.remove(item);
+        return this;
+    }
+
+    public ListWrapper<T> removeAt(int index) {
+        this.list.remove(index);
+        return this;
+    }
+
+    public ListWrapper<T> remove(ActionCallback2<Integer, T, Boolean> remover) {
+        if (remover == null) return this;
+
+        int size = list.size();
+
+        for (int i = 0; i < size; i++) {
+            Boolean rem = remover.invoke(i, list.get(i));
+            if (rem == null) rem = false;
+
+            if (rem) {
+                removeAt(i);
+                size--;
+            }
+        }
+
+        return this;
+    }
+    //endregion
+
+    //region processing methods
+    public ListWrapper<T> filter(ActionCallback<T, Boolean> action) {
+        if (action == null) return this;
+
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            Boolean remove = action.invoke(list.get(i));
+            if (remove == null) remove = false;
+            if (remove) {
+                list.remove((int) i);
+                size--;
+            }
+        }
+        return this;
+    }
+
+    public <M> ListWrapper<T> map(ActionCallback<T, M> action, ResultCallback<List<M>> result) {
+        return map(new ArrayList<>(list.size()), action, result);
+    }
+
+    public <M> ListWrapper<T> map(List<M> outList, ActionCallback<T, M> action, ResultCallback<List<M>> result) {
+        if (outList == null || action == null || result == null) return this;
+
+        for (T t : list) {
+            M value = action.invoke(t);
+            outList.add(value);
+        }
+
+        result.invoke(outList);
+
+        return this;
+    }
+
+    public ListWrapper<T> performAction(ActionCallback<ListWrapper<T>, Void> action) {
+        if (action != null)
+            action.invoke(this);
+        return this;
+    }
+    //endregion
+
+    public List<T> getList() {
+        return list;
+    }
+}
---
+++ GMUtilsAndroid/GMUtils/src/main/java/gmutils/collections/MapWrapper.java
@@ -0,0 +1,149 @@
+package gmutils.collections;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import gmutils.listeners.ActionCallback;
+import gmutils.listeners.ActionCallback2;
+import gmutils.listeners.ResultCallback;
+
+
+public class MapWrapper<K, V> {
+
+    public static <K, V> MapWrapper<K, V> create(Class<K> kClass, Class<V> vClass) {
+        return new MapWrapper<>(null);
+    }
+
+    public static <K, V> MapWrapper<K, V> create(Map<K, V> map) {
+        return new MapWrapper<>(map);
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    private final Map<K, V> map;
+
+    private MapWrapper(Map<K, V> map) {
+        if (map == null) {
+            this.map = new HashMap<>();
+        } else {
+            this.map = map;
+        }
+    }
+
+    //region add methods
+    public MapWrapper<K, V> add(K key, V value) {
+        this.map.put(key, value);
+        return this;
+    }
+
+    public MapWrapper<K, V> add(Map<K, V> otherMap) {
+        this.map.putAll(otherMap);
+        return this;
+    }
+
+    public MapWrapper<K, V> add(List<K> keys, List<V> value) {
+        if (keys == null || value == null || keys.size() != value.size())
+            throw new IllegalArgumentException("Keys must has corresponding Values");
+
+        for (int i = 0; i < keys.size(); i++) {
+            map.put(keys.get(i), value.get(i));
+        }
+
+        return this;
+    }
+
+    public MapWrapper<K, V> add(List<DataGroup.Two<K, V>> items) {
+        if (items == null) return this;
+
+        for (DataGroup.Two<K, V> item : items) {
+            map.put(item.value1, item.value2);
+        }
+
+        return this;
+    }
+
+    public MapWrapper<K, V> add(List<K> keys, ActionCallback<K, V> adder) {
+        if (keys == null || keys.size() == 0 || adder == null) return this;
+
+        for (K key : keys) {
+            V value = adder.invoke(key);
+            add(key, value);
+        }
+
+        return this;
+    }
+    //endregion
+
+    //region remove methods
+    public MapWrapper<K, V> remove(K key) {
+        this.map.remove(key);
+        return this;
+    }
+
+    public MapWrapper<K, V> remove(ActionCallback2<K, V, Boolean> remover) {
+        if (remover == null) return this;
+
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            Boolean rem = remover.invoke(entry.getKey(), entry.getValue());
+            if (rem == null) rem = false;
+
+            if (rem)
+                map.remove(entry.getKey());
+        }
+
+        return this;
+    }
+    //endregion
+
+    //region processing methods
+    public MapWrapper<K, V> filter(ActionCallback2<K, V, Boolean> action) {
+        if (action == null) return this;
+
+        LinkedList<K> keysTobeRemove = new LinkedList<>();
+
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            Boolean remove = action.invoke(entry.getKey(), entry.getValue());
+            if (remove == null) remove = false;
+            if (remove) {
+                keysTobeRemove.addLast(entry.getKey());
+            }
+        }
+
+        for (K key : keysTobeRemove) {
+            map.remove(key);
+        }
+
+        return this;
+    }
+
+    public <Vn> MapWrapper<K, V> map(ActionCallback2<K, V, Vn> action, ResultCallback<Map<K, Vn>> result) {
+        return map(new HashMap<>(), action, result);
+    }
+
+    public <Vn> MapWrapper<K, V> map(Map<K, Vn> outMap, ActionCallback2<K, V, Vn> action, ResultCallback<Map<K, Vn>> result) {
+        if (action == null || result == null) return this;
+
+        for (Map.Entry<K, V> entry : map.entrySet()) {
+            Vn newValue = action.invoke(entry.getKey(), entry.getValue());
+            outMap.put(entry.getKey(), newValue);
+        }
+
+        result.invoke(outMap);
+
+        return this;
+    }
+
+    public MapWrapper<K, V> performAction(ActionCallback<MapWrapper<K, V>, Void> action) {
+        if (action != null)
+            action.invoke(this);
+        return this;
+    }
+    //endregion
+
+    public Map<K, V> getMap() {
+        return map;
+    }
+}
