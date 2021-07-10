7ae87ce02c50e5734d3ffe4d5d59491bf2db752c
ahmed_hammouda_ogtech <ahmed0012012>
2021-07-10 03:27:52 +0200

    DecimalRangeFilter

--- GMUtilsAndroid/GMUtils_Example/src/main/java/com/blogspot/gm4s/gmutileexample/activities/ColorPickerActivity.kt
+++ GMUtilsAndroid/GMUtils_Example/src/main/java/com/blogspot/gm4s/gmutileexample/activities/ColorPickerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import com.blogspot.gm4s.gmutileexample.R
+import gmutils.inputFilters.DecimalRangeFilter
 import gmutils.ui.customViews.ColorPicker
 
 class ColorPickerActivity : AppCompatActivity() {
@@ -20,5 +21,12 @@ class ColorPickerActivity : AppCompatActivity() {
 
             //colorPicker.setupColorsRecyclerView(num)
         }
+
+        number.filters = arrayOf(
+            DecimalRangeFilter(
+                0.0,
+                1.0
+            )
+        )
     }
 }
