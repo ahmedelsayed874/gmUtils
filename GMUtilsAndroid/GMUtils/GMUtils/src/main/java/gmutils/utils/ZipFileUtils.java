51ab180666cdead2a354235cd9a69af144cf9949
ahmedelsayed874 <a.elsayedabdo@gmail.com>
2023-11-21 14:37:26 +0200

    new class for Zip utils: allow sync methods

--- GMUtilsAndroid/GMUtils/GMUtils/src/main/java/gmutils/utils/ZipFileUtils.java
+++ GMUtilsAndroid/GMUtils/GMUtils/src/main/java/gmutils/utils/ZipFileUtils.java
@@ -33,42 +33,52 @@ public class ZipFileUtils {
             File rootDir,
             ResultCallback<Error> onComplete
     ) {
+        BackgroundTask.run(() -> compressSync(
+                outZipFile,
+                rootDir
+        ), (e) -> {
+            if (onComplete != null) onComplete.invoke(e);
+        });
+    }
+
+    public Error compressSync(
+            File outZipFile,
+            File rootDir
+    ) {
         if (!outZipFile.exists()) {
             try {
                 outZipFile.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
-                if (onComplete != null) onComplete.invoke(new Error(e.getMessage()));
-                return;
+                return new Error(e.getMessage());
             }
         }
 
         if (!rootDir.isDirectory()) {
-            if (onComplete != null) onComplete.invoke(new Error("rootDir must be directory"));
-            return;
+            return new Error("rootDir must be directory");
         }
 
-        BackgroundTask.run(() -> {
-            List<File> files = new ArrayList<>();
+        List<File> files = new ArrayList<>();
 
-            List<File> dirs = new ArrayList<>();
-            dirs.add(rootDir);
+        List<File> dirs = new ArrayList<>();
+        dirs.add(rootDir);
 
-            int d = 0;
-            for (; d < dirs.size(); d++) {
-                File dir = dirs.get(d);
-                File[] subFiles = dir.listFiles();
-                if (subFiles != null) {
-                    for (File sf : subFiles) {
-                        if (sf.isFile()) files.add(sf);
-                        else if (sf.isDirectory()) dirs.add(sf);
-                    }
+        int d = 0;
+        for (; d < dirs.size(); d++) {
+            File dir = dirs.get(d);
+            File[] subFiles = dir.listFiles();
+            if (subFiles != null) {
+                for (File sf : subFiles) {
+                    if (sf.isFile()) files.add(sf);
+                    else if (sf.isDirectory()) dirs.add(sf);
                 }
             }
+        }
 
-            compress(outZipFile, rootDir, files, onComplete);
-        }, null);
+        return compressSync(outZipFile, rootDir, files);
     }
+
+    //-------------------------------------------------
 
     public void compress(
             File outZipFile,
@@ -76,104 +86,112 @@ public class ZipFileUtils {
             List<File> toCompressFiles,
             ResultCallback<Error> onComplete
     ) {
+        BackgroundTask.run(() -> compressSync(
+                outZipFile,
+                rootDir,
+                toCompressFiles
+        ), (e) -> {
+            if (onComplete != null) onComplete.invoke(e);
+        });
+    }
+
+    public Error compressSync(
+            File outZipFile,
+            File rootDir,
+            List<File> toCompressFiles
+    ) {
         if (!outZipFile.exists()) {
             try {
                 outZipFile.createNewFile();
             } catch (IOException e) {
                 e.printStackTrace();
-                if (onComplete != null) onComplete.invoke(new Error(e.getMessage()));
-                return;
+                return new Error(e.getMessage());
             }
         }
 
         if (!rootDir.isDirectory()) {
-            if (onComplete != null) onComplete.invoke(new Error("rootDir must be directory"));
-            return;
+            return new Error("rootDir must be directory");
         }
 
-        BackgroundTask.run(() -> {
-            OutputStream zipFileOutputStream = null;
-            ZipOutputStream zipStream = null;
+        OutputStream zipFileOutputStream = null;
+        ZipOutputStream zipStream = null;
+        try {
+            zipFileOutputStream = new FileOutputStream(outZipFile);
+            zipStream = new ZipOutputStream(zipFileOutputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return new Error(e.getMessage());
+        } finally {
+            if (zipFileOutputStream != null) {
+                try {
+                    zipFileOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (zipStream != null) {
+                try {
+                    zipStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        final String rootDirPath = rootDir.getAbsolutePath() + "/";
+
+        for (File file : toCompressFiles) {
+            String entryName = file.getAbsolutePath().replace(rootDirPath, "");
+
+            ZipEntry zipEntry = new ZipEntry(entryName);
+            FileInputStream fileInputStream = null;
             try {
-                zipFileOutputStream = new FileOutputStream(outZipFile);
-                zipStream = new ZipOutputStream(zipFileOutputStream);
-            } catch (FileNotFoundException e) {
+                zipStream.putNextEntry(zipEntry);
+
+                fileInputStream = new FileInputStream(file);
+                var length = 0;
+                byte[] buffer = new byte[1024];
+                while ((length = fileInputStream.read(buffer)) > 0) {
+                    zipStream.write(buffer, 0, length);
+                }
+
+                fileInputStream.close();
+                zipStream.closeEntry();
+            } catch (Exception e) {
                 e.printStackTrace();
                 return new Error(e.getMessage());
             } finally {
-                if (zipFileOutputStream != null) {
+                if (fileInputStream != null) {
                     try {
-                        zipFileOutputStream.close();
+                        fileInputStream.close();
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
-                if (zipStream != null) {
-                    try {
-                        zipStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            }
-
-            final String rootDirPath = rootDir.getAbsolutePath() + "/";
-
-            for (File file : toCompressFiles) {
-                String entryName = file.getAbsolutePath().replace(rootDirPath, "");
-
-                ZipEntry zipEntry = new ZipEntry(entryName);
-                FileInputStream fileInputStream = null;
                 try {
-                    zipStream.putNextEntry(zipEntry);
-
-                    fileInputStream = new FileInputStream(file);
-                    var length = 0;
-                    byte[] buffer = new byte[1024];
-                    while ((length = fileInputStream.read(buffer)) > 0) {
-                        zipStream.write(buffer, 0, length);
-                    }
-
-                    fileInputStream.close();
-                    zipStream.closeEntry();
-                } catch (Exception e) {
+                    zipFileOutputStream.close();
+                } catch (IOException e) {
                     e.printStackTrace();
-                    return new Error(e.getMessage());
-                } finally {
-                    if (fileInputStream != null) {
-                        try {
-                            fileInputStream.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    try {
-                        zipFileOutputStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        zipStream.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                }
+                try {
+                    zipStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
+        }
 
-            try {
-                zipStream.finish();
-                zipStream.close();
-                zipFileOutputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return new Error(e.getMessage());
-            }
+        try {
+            zipStream.finish();
+            zipStream.close();
+            zipFileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new Error(e.getMessage());
+        }
 
-            return null;
-        }, (e) -> {
-            if (onComplete != null) onComplete.invoke(e);
-        });
+        return null;
     }
 
     //----------------------------------------------------------------------------------------------
@@ -183,61 +201,68 @@ public class ZipFileUtils {
             File outputDir,
             ResultCallback<Error> onComplete
     ) {
-        if (!outputDir.isDirectory()) {
-            if (onComplete != null) onComplete.invoke(new Error("outputDir must be directory"));
-            return;
-        }
-
-        BackgroundTask.run(() -> {
-            FileOutputStream fileOutputStream = null;
-
-            try (fileInputStream; ZipInputStream zipInputStream = new ZipInputStream(fileInputStream)) {
-                ZipEntry zipEntry = null;
-
-                while ((zipEntry = zipInputStream.getNextEntry()) != null) {
-                    int lastSlash = zipEntry.getName().lastIndexOf("/");
-                    if (lastSlash >= 0) {
-                        String dirPath = zipEntry.getName().substring(0, lastSlash);
-                        new File(outputDir, dirPath).mkdirs();
-                    }
-
-                    File outFile = new File(outputDir, zipEntry.getName());
-                    if (!outFile.exists()) try {
-                        outFile.createNewFile();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        return new Error(e.getMessage());
-                    }
-
-                    fileOutputStream = new FileOutputStream(outFile);
-
-                    //-------------------------------------------------------------
-
-                    byte[] buffer = new byte[1024];
-                    int length = 0;
-                    while ((length = zipInputStream.read(buffer)) > 0) {
-                        fileOutputStream.write(buffer, 0, length);
-                    }
-
-                    zipInputStream.closeEntry();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return new Error(e.getMessage());
-            } finally {
-                if (fileOutputStream != null) {
-                    try {
-                        fileOutputStream.flush();
-                        fileOutputStream.close();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            return null;
-        }, (e) -> {
+        BackgroundTask.run(() -> extractSync(
+                fileInputStream,
+                outputDir
+        ), (e) -> {
             if (onComplete != null) onComplete.invoke(e);
         });
+    }
+
+    public Error extractSync(
+            InputStream fileInputStream,
+            File outputDir
+    ) {
+        if (!outputDir.isDirectory()) {
+            return new Error("outputDir must be directory");
+        }
+
+        FileOutputStream fileOutputStream = null;
+
+        try (fileInputStream; ZipInputStream zipInputStream = new ZipInputStream(fileInputStream)) {
+            ZipEntry zipEntry = null;
+
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                int lastSlash = zipEntry.getName().lastIndexOf("/");
+                if (lastSlash >= 0) {
+                    String dirPath = zipEntry.getName().substring(0, lastSlash);
+                    new File(outputDir, dirPath).mkdirs();
+                }
+
+                File outFile = new File(outputDir, zipEntry.getName());
+                if (!outFile.exists()) try {
+                    outFile.createNewFile();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return new Error(e.getMessage());
+                }
+
+                fileOutputStream = new FileOutputStream(outFile);
+
+                //-------------------------------------------------------------
+
+                byte[] buffer = new byte[1024];
+                int length = 0;
+                while ((length = zipInputStream.read(buffer)) > 0) {
+                    fileOutputStream.write(buffer, 0, length);
+                }
+
+                zipInputStream.closeEntry();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Error(e.getMessage());
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.flush();
+                    fileOutputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return null;
     }
 }
