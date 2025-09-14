730ef9b9c939746cdcfe6c9da75e564f1f120a47
ahmed_hammouda <ahmed_hammouda@ogtech.com>
2025-09-14 12:57:25 +0300

    string_set.dart: allow add more locale

--- GMUtilsFlutter/gmutils_flutter/lib/zgmutils/utils/string_set.dart
+++ GMUtilsFlutter/gmutils_flutter/lib/zgmutils/utils/string_set.dart
@@ -2,9 +2,12 @@
 class StringSet {
   Map<String, String> _strings = {};
 
-  StringSet(String en, [String? ar]) {
+  StringSet(String en, [String? ar, Map<String, String>? more]) {
     _strings['en'] = en;
     _strings['ar'] = (ar ?? en);
+    if (more?.isNotEmpty == true) {
+      _strings.addAll(more!);
+    }
   }
 
   StringSet.multiple(Map<String, String> strings) {
